1e2fa2371527f121617d9fae97c9fdca93bb1efe
Quang Thang <thang@thangqt.com>
2024-10-06 20:50:04 +0700

    Move existing functionality in ConsoleView to ConsoleController

--- src/main/java/com/thangqt/libman/LibraryManagementApp.java
+++ src/main/java/com/thangqt/libman/LibraryManagementApp.java
@@ -1,5 +1,6 @@
 package com.thangqt.libman;
 
+import com.thangqt.libman.controller.ConsoleController;
 import com.thangqt.libman.service.*;
 import com.thangqt.libman.view.cli.ConsoleView;
 
@@ -7,16 +8,15 @@ import java.sql.SQLException;
 
 public class LibraryManagementApp {
     public static void main(String[] args) throws SQLException {
-        // Initialize the ServiceFactory
         ServiceFactory serviceFactory = ServiceFactory.getInstance();
-
-        // Get the managers
         UserManager userManager = serviceFactory.getUserManager();
         MaterialManager materialManager = serviceFactory.getMaterialManager();
         LoanManager loanManager = serviceFactory.getLoanManager();
 
+        ConsoleController consoleController = new ConsoleController(userManager, materialManager, loanManager);
+
         // Create and start the ConsoleView
-        ConsoleView consoleView = new ConsoleView(userManager, materialManager, loanManager);
+        ConsoleView consoleView = new ConsoleView(consoleController);
         consoleView.start();
     }
 }
