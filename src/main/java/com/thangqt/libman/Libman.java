96748f33bbff14e41aa69e387644c74fd19d87de
Quang Thang <thang@thangqt.com>
2024-11-05 00:50:10 +0700

    parsing args to launch

--- src/main/java/com/thangqt/libman/Libman.java
+++ src/main/java/com/thangqt/libman/Libman.java
@@ -3,20 +3,24 @@ package com.thangqt.libman;
 import com.thangqt.libman.controller.ConsoleController;
 import com.thangqt.libman.service.*;
 import com.thangqt.libman.view.ConsoleView;
+import com.thangqt.libman.view.GraphicalView.GraphicalView;
+import javafx.application.Application;
+
 import java.sql.SQLException;
 
-public class LibraryManagementApp {
+public class Libman {
   public static void main(String[] args) throws SQLException {
     ServiceFactory serviceFactory = ServiceFactory.getInstance();
     UserManager userManager = serviceFactory.getUserManager();
     MaterialManager materialManager = serviceFactory.getMaterialManager();
     LoanManager loanManager = serviceFactory.getLoanManager();
 
-    ConsoleController consoleController =
-        new ConsoleController(userManager, materialManager, loanManager);
-
-    // Create and start the ConsoleView
-    ConsoleView consoleView = new ConsoleView(consoleController);
-    consoleView.start();
+    if (args.length > 0 && args[0].equalsIgnoreCase("cli")) {
+      ConsoleController consoleController = new ConsoleController(userManager, materialManager, loanManager);
+      ConsoleView consoleView = new ConsoleView(consoleController);
+      consoleView.start();
+    } else {
+      Application.launch(GraphicalView.class, args);
+    }
   }
 }
